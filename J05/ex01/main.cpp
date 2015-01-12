0195235ccc501fd2fd56c1328ccf17b1a95211ca
gmp89 <gm.petrov@yahoo.fr>
2015-01-12 21:30:10 +0100

    update

--- J05/ex01/main.cpp
+++ J05/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gmp <gmp@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: gpetrov <gpetrov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/12 13:22:50 by gmp               #+#    #+#             */
-/*   Updated: 2015/01/12 18:47:04 by gmp              ###   ########.fr       */
+/*   Updated: 2015/01/12 21:25:48 by gpetrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,19 @@
 
 int		main(void){
 
-	Bureaucrat jean("Jean", 4);
+	Bureaucrat jean("Jean", 42);
 	Form form("Achat Voiture", 3, 10);
 
+	std::cout << "Going to call beSigned() without signForm()" << std::endl;
+	form.beSigned(jean);
 	std::cout << form << std::endl;
 
+	std::cout << "Now Calling signForm()" << std::endl;
 	jean.signForm(form);
 
+	Bureaucrat boss("Boss", 1);
+	std::cout << "Now the boss is going to handle the case" << std::endl;
+	boss.signForm(form);
 	std::cout << form << std::endl;
-	// Bureaucrat georges("Georges", 149);
-	// georges--;
-	// std::cout << georges << std::endl;
-	// georges--;
-	// std::cout << georges << std::endl;
-
-	// std::cout << std::endl;
-
-	// Bureaucrat jean("Jean", 2);
-	// jean++;
-	// std::cout << jean << std::endl;
-	// jean++;
-	// std::cout << jean << std::endl;
-
 	return 0;
 }
